60c473d68a22d99a0391434860e425dad6b75513
Corey Roberts <crobrts22@stu.jsu.edu>
2024-03-22 01:10:12 -0500

    Started on Punch Adjust method. Worked on logic for clock in early and clock out grace and dock penalty, Corrected printOriginal()

--- src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Punch.java
@@ -2,6 +2,8 @@ package edu.jsu.mcis.cs310.tas_sp24;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 /**
  *
@@ -65,22 +67,88 @@ public class Punch {
         return adjustmenttype;
     }
     
+    //Adjust Method
+    public void adjust(Shift s){
+        //Extract time from punches original timestamp
+        LocalTime punchTime = this.originaltimestamp.toLocalTime();
+        
+        //Determine punch clock in or clock out
+        boolean isClockIn = this.punchtype == EventType.CLOCK_IN;
+        boolean isClockOut = this.punchtype == EventType.CLOCK_OUT;
+        
+        System.out.println("Original punch time" + this.originaltimestamp);
+        System.out.println("Punchtype " + (isClockIn ? "Clock In" : "Clock Out"));
+        
+        //---Adjustment logic rules start---
+        // Identify early clock in
+        if (isClockIn && punchTime.isBefore(s.getShiftstart())){
+            //Adjustment type: Early clock in 
+            // punchTime.plusMinutes(adds15min) Calculates new time if new time passes (isAfter) s.getShiftstart
+            //time snaps back to Shift_Start
+            if (punchTime.plusMinutes(s.getRoundinterval()).isAfter(s.getShiftstart())){
+                this.adjustedtimestamp = LocalDateTime.of(this.originaltimestamp.toLocalDate(), s.getShiftstart());
+                this.adjustmenttype = PunchAdjustmentType.SHIFT_START;
+            }
+        }
+        
+        //Late clock in adjustment (Grace Period or Dock Penalty
+        if (isClockIn && punchTime.isAfter(s.getShiftstart())){
+            if (punchTime.isBefore(s.getShiftstart().plusMinutes(s.getGraceperiod()))){
+                //Within grace period 5mins late shift back to Shift Start
+                this.adjustedtimestamp = LocalDateTime.of(this.originaltimestamp.toLocalDate(), s.getShiftstart());
+                this.adjustmenttype = PunchAdjustmentType.SHIFT_START;
+            } else if (punchTime.isBefore(s.getShiftstart().plusMinutes(s.getDockpenalty()))){
+                //past grace period but before dock penalty
+                this.adjustedtimestamp = LocalDateTime.of(this.originaltimestamp.toLocalDate(), s.getShiftstart().plusMinutes(s.getDockpenalty()));
+                this.adjustmenttype = PunchAdjustmentType.SHIFT_DOCK;
+            }
+        }
+        
+        //Early clock out adjustment
+        if (isClockOut && punchTime.isBefore(s.getShiftstop())){
+            //Check if within grace period 5mins clockout early
+            if (punchTime.plusMinutes(s.getGraceperiod()).isAfter(s.getShiftstop())){
+                //considered within grace period shift time back to Shiftstop
+                this.adjustedtimestamp = LocalDateTime.of(this.originaltimestamp.toLocalDate(), s.getShiftstop());
+                this.adjustmenttype = PunchAdjustmentType.SHIFT_STOP;
+            } else if (punchTime.plusMinutes(s.getDockpenalty()).isBefore(s.getShiftstop())){
+                //adjust time stamp to dock penalty
+                this.adjustedtimestamp = LocalDateTime.of(this.originaltimestamp.toLocalDate(), s.getShiftstop().minusMinutes(s.getDockpenalty()));
+                this.adjustmenttype = PunchAdjustmentType.SHIFT_DOCK;
+            }
+            
+        }
+        
+        //Lunch adjustments
+        
+        
+        //interval round
+        
+        
+        //none
+        
+        
+        
+        
+    }
+    
     
     // PrintOriginal 
     public String printOriginal(){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MM/dd/yyyy HH:mm:ss");
         String formattedDate = this.originaltimestamp.format(formatter).toUpperCase();
-    
-        //using toString method of punchType (EventType enum)
         String punchTypeString = this.punchtype.toString();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("#")
+                .append(this.badge.getId())
+                .append(" ")
+                .append(punchTypeString)
+                .append(": ")
+                .append(formattedDate);
     
-        return String.format("#%s %s: %s", this.badge.getId(), punchTypeString, formattedDate);
-    }
-    
-    @Override
-    public String toString() {
-        return printOriginal();
+        return sb.toString();
     }
 
-    
 }
